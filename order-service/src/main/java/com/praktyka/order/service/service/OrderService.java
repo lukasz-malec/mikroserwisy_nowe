00e6f5407cdbb027cbcd717f1400a7b6f4f8dd80
lukasz1_malec <lmalec493@gmail.com>
2024-07-31 13:54:11 +0200

    Zmiany w instancji WebClient

--- order-service/src/main/java/com/praktyka/order/service/service/OrderService.java
+++ order-service/src/main/java/com/praktyka/order/service/service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
 
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder  webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -42,8 +42,8 @@ public class OrderService {
                 .toList();
 
 
-        InventoryResponse[] inventoryResponsArray = webClient.get()
-                .uri("http://localhost:8083/api/inventory",
+        InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder ->uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
